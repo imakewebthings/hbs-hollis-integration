4d1ef646502d2df3ea79838517bcfa822d1618b3
imakewebthings <caleb@imakewebthings.com>
2014-02-27 20:49:12 -0800

    Another interpage transition

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -1,13 +1,17 @@
 (function() {
   $(document)
-    .on('click', '.search-by a', function(event) {
+    .on('click', '.search-by a', function() {
       $('.fat-inner p').slideUp();
       $('main').animate({ opacity: 0.3 });
     })
-    .on('click', '.search-list a', function(event) {
+    .on('click', '.search-list a', function() {
       $('.stackview-container, #publication').animate({ opacity: 0.3 });
+      $('.search-by:not(header .search-by)').slideUp();
     })
-    .on('click', '.stack-item', function(event) {
+    .on('click', '.stack-item', function() {
       $('.publication-container').animate({ opacity: 0.3 });
+    })
+    .on('page:restore', function() {
+      $('.fat-inner p, .search-by:not(header .search-by)').slideDown();
     });
 })();
